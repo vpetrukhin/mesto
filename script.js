7a8b37d25ba400cb43dde178283ef6764f917a9e
Vasya <vpetruin@inbox.ru>
2021-01-28 16:09:21 +0300

    feuture: Реализовано сохранение введеных в форму данных

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 'use strict';
 
-const btnClose = document.querySelector('.popup__btn-close'),
+const modal = document.querySelector('.popup'),
+      popupForm = document.querySelector('.popup__form'),
+      btnClose = document.querySelector('.popup__btn-close'),
       btnEdit = document.querySelector('.profile__edit-btn'),
-      modal = document.querySelector('.popup');
+      profileName = document.querySelector('.profile__name'),
+      profileJob = document.querySelector('.profile__job'),
+      popupInputName = modal.querySelector('#name'),
+      popupInputJob = modal.querySelector('#job');
+
+let nameValue = profileName.textContent;
+let jobValue = profileJob.textContent;
+
+// console.log(btnEdit);
 
 function modalOpen() {
+  popupInputName.value = nameValue;
+  popupInputJob.value = jobValue;
   modal.classList.add('popup_opened');
 }
 
@@ -12,5 +24,14 @@ function modalClose() {
   modal.classList.remove('popup_opened');
 }
 
+function formSubmitHandler (e) {
+  e.preventDefault();
+  
+  profileName.textContent = popupInputName.value;
+  profileJob.textContent = popupInputJob.value;
+  modalClose();
+}
+
 btnEdit.addEventListener('click', modalOpen);
 btnClose.addEventListener('click', modalClose);
+popupForm.addEventListener('submit', formSubmitHandler);
