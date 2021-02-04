0e5e8dafce4ac1da1a394469b3c9363189fcbb70
Vasya <vpetruin@inbox.ru>
2021-02-04 23:42:29 +0300

    feutures: Сверстана форма добавления фотографии

--- js/script.js
+++ js/script.js
@@ -9,6 +9,35 @@ const modal = document.querySelector('.popup'),
       popupInputName = modal.querySelector('#name'),
       popupInputJob = modal.querySelector('#job');
 
+//Массив с карточками
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 // Функции
 
 // Открытие попапа
